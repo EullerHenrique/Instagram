f2021c6aee8fa46f90e218026f92780a362e0522
EullerHenrique <eullerhenrique@outlook.com>
2021-01-26 16:50:09 -0300

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { AcessoComponent } from './acesso/acesso.component';
 import { BannerComponent } from './acesso/banner/banner.component';
 import { LoginComponent } from './acesso/login/login.component';
+import { CadastroComponent } from './acesso/cadastro/cadastro.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, AcessoComponent, BannerComponent, LoginComponent ],
+  declarations: [ AppComponent, AcessoComponent, BannerComponent, LoginComponent, CadastroComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
