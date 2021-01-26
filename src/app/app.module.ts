dd0ea547078d929ffbe9db78bfa00cfa58acf8e8
EullerHenrique <eullerhenrique@outlook.com>
2021-01-26 16:06:48 -0300

    feat: modifies content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AcessoComponent } from './acesso/acesso.component';
-import { BannerComponent } from './acesso//banner/banner.component';
+import { BannerComponent } from './acesso/banner/banner.component';
 import { LoginComponent } from './acesso/login/login.component';
 
 @NgModule({
