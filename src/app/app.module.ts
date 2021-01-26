e0be27919b687bbe48c20522de9424b666f199d0
EullerHenrique <eullerhenrique@outlook.com>
2021-01-26 15:40:29 -0300

    feat: adds new content in app.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { AcessoComponent } from './acesso/acesso.component';
-import { BannerComponent } from './banner/banner.component';
-import { LoginComponent } from './login/login.component';
+import { BannerComponent } from './acesso//banner/banner.component';
+import { LoginComponent } from './acesso/login/login.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, AcessoComponent, BannerComponent, LoginComponent ],
+  declarations: [ AppComponent, AcessoComponent, BannerComponent, LoginComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
