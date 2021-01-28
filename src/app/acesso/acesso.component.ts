c41d0ed9358169075e70d0589c2b24c8e1340008
EullerHenrique <eullerhenrique@outlook.com>
2021-01-28 16:47:46 -0300

    1 file modified

--- src/app/acesso/acesso.component.ts
+++ src/app/acesso/acesso.component.ts
@@ -1,9 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { transition, animate, trigger, state, style } from '@angular/animations';
 
 @Component({
   selector: 'app-acesso',
   templateUrl: './acesso.component.html',
-  styleUrls: ['./acesso.component.css']
+  styleUrls: ['./acesso.component.css'],
+  animations: [ 
+    trigger('animacao-banner', [
+      state('criado', style({
+        opacity: 1
+    })),
+    transition('void , => criado',
+     [ style( {opacity: 0 } ),
+       animate( '500ms 1s ease-in-out') //duração, delay e aceleração
+      ])
+    ])
+  ]
 })
 export class AcessoComponent implements OnInit {
 
