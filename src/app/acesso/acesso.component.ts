c8dbff0ea2e8d699aa78a02eb3f900bed70113f7
EullerHenrique <eullerhenrique@outlook.com>
2021-01-28 17:01:50 -0300

    2 files modified

--- src/app/acesso/acesso.component.ts
+++ src/app/acesso/acesso.component.ts
@@ -14,12 +14,22 @@ import { transition, animate, trigger, state, style } from '@angular/animations'
      [ style( {opacity: 0, transform: 'translate(-50px,0) ' } ),
        animate( '500ms 0s ease-in-out') //duração, delay e aceleração
       ])
+    ]),
+     trigger('animacao-painel', [
+      state('criado', style({
+        opacity: 1
+    })),
+    transition('void  => criado',
+     [ style( {opacity: 0, transform: 'translate(50px,0) ' } ),
+       animate( '500ms 0s ease-in-out') //duração, delay e aceleração
+      ])
     ])
   ]
 })
 export class AcessoComponent implements OnInit {
 
   public estadoBanner: string = 'criado';
+  public estadoPainel: string = 'criado';
 
   constructor() { }
 
