1f41ae6e4a41803759391a623b951aa061c0d529
EullerHenrique <eullerhenrique@outlook.com>
2021-01-31 15:50:17 -0300

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AcessoComponent } from './acesso/acesso.component';
@@ -11,7 +11,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, BrowserAnimationsModule ],
-  declarations: [ AppComponent, AcessoComponent, BannerComponent, LoginComponent, CadastroComponent ],
+  declarations: [ AppComponent, AcessoComponent, BannerComponent, LoginComponent, CadastroComponent, ReactiveFormsModule ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
