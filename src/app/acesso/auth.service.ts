e49e3375b2e5484bf6fd2b1c323d2e2486a5f082
EullerHenrique <eullerhenrique@outlook.com>
2021-01-31 20:57:25 -0300

    1 file modified

--- src/app/acesso/auth.service.ts
+++ src/app/acesso/auth.service.ts
@@ -1,10 +1,16 @@
+import * as firebase from "firebase";
 import { Usuario } from "./usuario.model";
 
 export class Auth{
 
   public cadastrarUsuario(usuario: Usuario){
 
-    
+    firebase.auth().createUserWithEmailAndPassword(usuario.email, usuario.senha).then((resposta: any) => {
+      console.log(resposta);
+    })
+    .catch((error: Error) => {
+      console.log(error);
+    })
 
   }
 
