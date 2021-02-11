b01fc52fe133d2f48c2ce5dc6961ec3446ff4bb0
EullerHenrique <eullerhenrique@outlook.com>
2021-02-11 20:12:52 -0300

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,11 @@ import { LoginComponent } from './acesso/login/login.component';
 import { CadastroComponent } from './acesso/cadastro/cadastro.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Auth } from './acesso/auth.service';
+import { HomeComponent } from './acesso/home/home.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, BrowserAnimationsModule,  ReactiveFormsModule ],
-  declarations: [ AppComponent, AcessoComponent, BannerComponent, LoginComponent, CadastroComponent ],
+  declarations: [ AppComponent, AcessoComponent, BannerComponent, LoginComponent, CadastroComponent, HomeComponent ],
   providers: [ Auth ],
   bootstrap:    [ AppComponent ]
 })
