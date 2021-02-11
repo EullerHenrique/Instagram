477fbb779fec06ff7251e25f6891514a74e80bfa
EullerHenrique <eullerhenrique@outlook.com>
2021-02-11 02:13:18 -0300

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,17 +15,21 @@ export class AppComponent implements OnInit{
 
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+   // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   var firebaseConfig = {
     apiKey: "AIzaSyBiSVAIcpOoCd-1-EsjMbWuzgW8nAMyN-Y",
     authDomain: "instagram-c254b.firebaseapp.com",
+    databaseURL: "https://instagram-c254b-default-rtdb.firebaseio.com",
     projectId: "instagram-c254b",
     storageBucket: "instagram-c254b.appspot.com",
     messagingSenderId: "662833096777",
-    appId: "1:662833096777:web:c9704957de5c732f8bf798",
-    measurementId: "G-91W9BZQ1N9"
+    appId: "1:662833096777:web:99e6c9644876ad168bf798",
+    measurementId: "G-KE9YQQ4TBT"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
 
   }
 
