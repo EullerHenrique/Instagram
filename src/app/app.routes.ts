c45893963c10bd0f3a550a494c6570c32fbfbe5d
EullerHenrique <eullerhenrique@outlook.com>
2021-02-12 17:04:10 -0300

    1 file modified

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { AcessoComponent } from './acesso/acesso.component';
-import { HomeComponent } from './acesso/home/home.component';
+import { HomeComponent } from './/home/home.component';
 
 export const ROUTES = [
 
