9d0b3c056721d6c93850a32a76fa24882ce1ebd4
EullerHenrique <eullerhenrique@outlook.com>
2021-02-13 17:11:41 -0300

    2 files modified

--- src/app/home/incluir-publicacao/incluir-publicacao.component.ts
+++ src/app/home/incluir-publicacao/incluir-publicacao.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-incluir-publicacao',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IncluirPublicacaoComponent implements OnInit {
 
+  public form: FormGroup = new FormGroup({
+    'titulo': new FormControl(null)
+  })
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public publicar(): void{
+
+    console.log('oioi')
+
   }
 
 }
