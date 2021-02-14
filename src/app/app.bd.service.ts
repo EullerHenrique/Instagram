fac2581559959275d4bdb5ca4a63c5a0cabf37f0
EullerHenrique <eullerhenrique@outlook.com>
2021-02-14 19:55:36 -0300

    1 file modified

--- src/app/app.bd.service.ts
+++ src/app/app.bd.service.ts
@@ -10,8 +10,23 @@ export class Bd {
     let imageName = Date.now();
     firebase.storage().ref()
     .child(`imagens/${imageName}`)
-    .put(publicacao.imagem);
-
+    .put(publicacao.imagem)
+    .on(firebase.storage.TaskEvent.STATE_CHANGED,
+      
+      //Acompanha pogresso do upload
+      (snapshot: any) => {
+        console.log(snapshot);
+      },
+      //Erro do upload
+      (error) => {
+        console.log(error);
+      },
+      //Finalização do upload
+      () => {
+        console.log('upload completo');
+      }
+    )
+    
     firebase.database().ref(`publicacoes/${btoa(publicacao.email)}`).push({titulo: publicacao.titulo});
   }
 }
