659909e9b84e781f00fe3be64d342240e3cc357f
EullerHenrique <eullerhenrique@outlook.com>
2021-02-15 22:06:59 -0300

    1 file modified

--- src/app/home/publicacoes/publicacoes.component.ts
+++ src/app/home/publicacoes/publicacoes.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Bd } from '../bd.service';
+import { firebase } from '@firebase/app';
+import '@firebase/auth';
 
 @Component({
   selector: 'app-publicacoes',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PublicacoesComponent implements OnInit {
 
-  constructor() { }
+  public email: string;
+
+  constructor(public bd: Bd) { }
 
   ngOnInit() {
+
+    firebase.auth().onAuthStateChanged((user)=>{
+      this.email=user.email;
+    }) 
+
+  }
+
+  public atualizarTimeline(): void{
+
+    this.bd.consultaPublicacoes(this.email)
+
   }
 
 }
